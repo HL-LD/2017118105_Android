084a1f067de1dad2d2d444ae9f9e08b43b715669
HLLD <2017118105@stu.hstc.edu.cn>
2019-09-18 15:20:06 +0800

    活动切换

---
+++ ActivityTest/app/src/main/java/com/example/activitytest/helloworld1.java
@@ -0,0 +1,26 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class helloworld1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(helloworld1.this,helloworld2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
